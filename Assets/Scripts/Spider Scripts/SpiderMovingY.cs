f8ea3fd80999e2769444a4cc873c129f6c14e2f9
nducthien <thien.chotrax23@gmail.com>
2019-04-23 08:11:44 +0700

    update game 2d

---
+++ Assets/Scripts/Spider Scripts/SpiderMovingY.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderMovingY : MonoBehaviour {
+
+    private Rigidbody2D enemyBody;
+    public bool checkBottom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveForeY = 0f;
+        if (checkBottom)
+        {
+            moveForeY = 2f;
+        } else
+        {
+            moveForeY = -2f;
+        }
+        enemyBody.velocity = new Vector2(0, transform.localScale.y) * moveForeY;
+    }
+
+    void OnCollisionEnter2D(Collision2D target)
+    {
+        if (target.gameObject.tag == "Ground")
+        {
+            checkBottom = false;
+        }
+
+        if (target.gameObject.tag == "minhi")
+        {
+            checkBottom = true;
+        }
+    }
+}
